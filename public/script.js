79b93a1c1b2b5b92f60e5fb77d246fc5fe4d5bf1
rauzola <raul_sigoli@hotmail.com>
2020-07-05 14:03:10 -0300

    Configurando o Babel

---
+++ public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Converter (transpilar) codigo do React para um codigo que o browser entenda
+ * Webpack: Pra cada tpo de arquivo (.js, .cs, .png) eu vou converter o codigo de uma maneira diferente.
+ *
+ * Loaders: babel-loader, css-loader, image-loader
+ */
