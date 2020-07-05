cb5aa51a643b50a71e8a22dd0f5ef6b3e0c68cef
rauzola <raul_sigoli@hotmail.com>
2020-07-05 14:55:28 -0300

    Propriedades

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>HomePage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Projects</li>
+          <li>GoStack</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   );
 }
